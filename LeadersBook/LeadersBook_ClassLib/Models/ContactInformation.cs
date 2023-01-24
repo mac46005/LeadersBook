164cd778956f72fb74718898a51c70a54fcc3590
Marco Preciado <mac46005@gmail.com>
2023-01-24 11:36:14 -0600

    Concrete Counseling

---
+++ LeadersBook/LeadersBook_ClassLib/Models/ContactInformation.cs
@@ -0,0 +1,17 @@
+﻿using LeadersBook_ClassLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadersBook_ClassLib.Models
+{
+    public class ContactInformation<T, U, V> : IContactInformation<T, U, V>
+    {
+        public List<IAddress<T, U, V>>? Addresses { get; set; }
+        public List<IPhoneNumber<T, U, V>>? PhoneNumbers { get;set; }
+        public U Model_ID { get;set; }
+        public T ID { get;set; }
+    }
+}
---
+++ LeadersBook/LeadersBook_ClassLib/Models/Counseling.cs
@@ -0,0 +1,19 @@
+﻿using LeadersBook_ClassLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadersBook_ClassLib.Models
+{
+    public class Counseling<T, U, V> : ICounseling<T, U, V>
+    {
+        public ISoldier<T, U>? Soldier { get; set; }
+        public DateTime? InitialCounselingDate { get;set; }
+        public DateTime? PerformanceCounselingDate { get;set; }
+        public DateTime? NextPerformanceCounselingDate { get;set; }
+        public U Model_ID { get;set; }
+        public T ID { get;set; }
+    }
+}
