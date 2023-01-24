689645006359dcd8c7b1a26dc1c6f6136d8f7119
Marco Preciado <mac46005@gmail.com>
2023-01-24 14:39:31 -0600

    Made personal data concrete classes

---
+++ LeadersBook/LeadersBook_ClassLib/Models/UniformInformation.cs
@@ -0,0 +1,19 @@
+﻿using LeadersBook_ClassLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadersBook_ClassLib.Models
+{
+    public class UniformInformation : IUniformInformation<int, int, ISoldier<int, int>>
+    {
+        public string? CoatSize { get; set; }
+        public string? TrouserSize { get;set; }
+        public string? HatSize { get;set; }
+        public string? BootSize { get;set; }
+        public int Model_ID { get;set; }
+        public int ID { get;set; }
+    }
+}
---
+++ LeadersBook/LeadersBook_ClassLib/Models/VehicleInformation.cs
@@ -0,0 +1,20 @@
+﻿using LeadersBook_ClassLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadersBook_ClassLib.Models
+{
+    public class VehicleInformation : IVehicleInformation<int, int, ISoldier<int, int>>
+    {
+        public string? Make { get; set; }
+        public string? Model { get;set; }
+        public string? Year { get;set; }
+        public string? Color { get;set; }
+        public string? Lic_Plate { get;set; }
+        public int Model_ID { get;set; }
+        public int ID { get;set; }
+    }
+}
