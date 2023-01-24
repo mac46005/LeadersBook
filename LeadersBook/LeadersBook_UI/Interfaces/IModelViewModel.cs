230a4a323d9b6187199b33109342c493b4a72d98
Marco Preciado <mac46005@gmail.com>
2023-01-24 07:55:51 -0600

    Thinking about the design of this application

---
+++ LeadersBook/LeadersBook_UI/Interfaces/IModelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DbManipulation_ClassLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadersBook_UI.Interfaces
+{
+    public interface IModelViewModel<T,U> where T : IModel<U>
+    {
+        T Model { get; set; }
+    }
+}
